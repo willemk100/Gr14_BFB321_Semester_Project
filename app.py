8b40b4b5f9697ca91523726c912afa7448b1d953
Ayden <u04867999@tuks.co.za>
2025-11-06 10:14:42 +0200

    Putting existing code into sections

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-#===========================================================
 # Main Application File: app.py
+#===========================================================
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
-
 
 app = Flask(__name__)
 app.secret_key = '12345'  # Needed for session management # I do not know what for
@@ -59,19 +58,21 @@ def login():
 #End of Login Page
 #===========================================================
 
+#For log out
+#===========================================================
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+#End of log out
+#===========================================================
 
-# Customer home page 
-@app.route('/customer_home')
-def customer_home():
-    if session.get('user_type') != 'customer':
-        return redirect(url_for('login'))
-
-    conn = get_db_connection()
-    vendors = conn.execute('SELECT * FROM vendor').fetchall()
-    conn.close()
-    return render_template('customer-main.html', vendors=vendors)
-    
-# Admin home page 
+#Now we move to different user sections
+ 
+#ADMIN SECTION!!!
+#************************************************************
+#Admin Home Page
+#===========================================================
 @app.route('/admin_home')
 def admin_home():
     if session.get('user_type') != 'admin':
@@ -80,9 +81,11 @@ def admin_home():
     vendors = conn.execute('SELECT * FROM vendor').fetchall()
     conn.close()
     return render_template('vendor_admin.html', vendors=vendors)
+#End of Admin Home Page
+#===============================================================
 
-####################################
-
+#New vendor page
+#===============================================================
 # Add vendor page
 @app.route('/add_vendor', methods=['GET', 'POST'])
 def add_vendor():
@@ -128,21 +131,32 @@ def add_vendor():
 
     return render_template('new_vendor.html')
 
-######################################
+#End of new vendor page
+#===============================================================
+#End of ADMIN SECTION!!!
+#***************************************************************
 
-# Vendor home page
-@app.route('/vendor_home')
-def vendor_home():
-    if session.get('user_type') != 'vendor':
+
+
+#CUSTOMER SECTION!!!
+#***************************************************************
+#Customer home page 
+#================================================================
+@app.route('/customer_home')
+def customer_home():
+    if session.get('user_type') != 'customer':
         return redirect(url_for('login'))
-    return render_template('vendor_main.html')
 
-#For log out
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('login'))
+    conn = get_db_connection()
+    vendors = conn.execute('SELECT * FROM vendor').fetchall()
+    conn.close()
+    return render_template('customer-main.html', vendors=vendors)
     
+#End of Customer home page
+#================================================================
+
+#Menu section
+#================================================================
 # For view menu of [vendor]
 @app.route('/vendor/<int:vendor_id>/menu')
 def vendor_menu(vendor_id):
@@ -163,28 +177,33 @@ def vendor_menu(vendor_id):
     categories = sorted({item['category'] for item in menu_items})
     return render_template('menu.html', menu_items=menu_items, selected_vendor=selected_vendor, categories = categories)
 
+#End of Menu section
+#================================================================
+#End of CUSTOMER SECTION!!!
+#***************************************************************
 
 
+#VENDOR SECTION!!!
+#***************************************************************
+# Vendor home page
+#===============================================================
+@app.route('/vendor_home')
+def vendor_home():
+    if session.get('user_type') != 'vendor':
+        return redirect(url_for('login'))
+    return render_template('vendor_main.html')
+#End of Vendor home page
+#===============================================================
+#End of VENDOR SECTION!!!
+#***************************************************************
+    
 
-
-
-
-# Run the app
+#Run the app
+#===========================================================
 if __name__ == '__main__':
     app.run(debug=True)
-
-#==========================================================
-#Vendor main 
-
-
-#End of Vendor Main 
-#==========================================================
+# End of app run
+#===========================================================
 
 
 
-#==========================================================
-#Vendor Analytics
-
-
-#End of Vendor Analytics ### this is a comment
-#==========================================================
